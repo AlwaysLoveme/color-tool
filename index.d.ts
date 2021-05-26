e816b4da52137b0fa4e35157951951f4b6a0632a
朱贤 <zhuxian@SKATTING.local>
2021-05-26 14:56:28 +0800

    add funcs

---
+++ index.d.ts
@@ -0,0 +1,12 @@
+/**  */
+export const colorHexToRgb: (hex: string) => number[];
+/**  */
+export const colorRgbToHex: (r: number, g: number, b: number) => string;
+/**  */
+export const colorRgbToHsl: (r: number, g: number, b: number) => number[];
+/**  */
+export const colorHslToRgb: (h: number, s: number, l: number) => number[];
+/**  */
+export const colorHsbToHsl: (h: number, s: number, b: number) => number[];
+/**  */
+export const colorHslToHsb: (h: number, s: number, l: number) => number[];
